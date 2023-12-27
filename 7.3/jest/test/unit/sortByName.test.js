8bb5b6feb395d6cb00220d4145f624aac69ef56d
Vitaliy Rogov <vv55rogov@gmail.com>
2023-12-27 10:43:40 +0600

    homework 7-03-1

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,5 +1,16 @@
 const sorting = require("../../app");
 
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverageFrom: [
+    '**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+  ],
+};
+
+module.exports = config;
+
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
     expect(
@@ -13,5 +24,45 @@ describe("Books names test suit", () => {
       "Волшебник изумрудного города",
       "Гарри Поттер",
     ]);
+
+    /* const input = [                      // вариант из лекции   
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ]
+
+    const output = sorting.sortByName(input)
+
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]
+
+    expect(output).toEqual(expected); */
+    
+  });
+
+  /* TestWatcher("Without param", () => {           // Второй вариант из лекции
+    expect(() => sorting.sortByName()).toThrow(TypeError)
+  }) */
+
+  it("Books have not been sorted", () => {
+    const input = [
+      "Волшебник изумрудного города",
+      "Волшебник изумрудного города",
+      "Волшебник изумрудного города",
+    ]
+
+    const output = sorting.sortByName(input)
+
+    const expected = [
+      "Волшебник изумрудного города",
+      "Волшебник изумрудного города",
+      "Волшебник изумрудного города",
+    ]
+
+    expect(output).toEqual(expected);
+    
   });
 });
