39b9e7055135bfd4ff69d81caf55519a4d97372b
Vitaliy Rogov <vv55rogov@gmail.com>
2024-01-09 22:51:10 +0600

    ver.1

--- 7.3/playwright/tests/Account.spec.js
+++ 7.3/playwright/tests/Account.spec.js
@@ -5,41 +5,22 @@ const {
     emailInvalid,
     passwordInvalid
   
-} = require("../user.js"); 
+} = require("../user.js");
 
 test.describe("Авторизация", () => {
   test('Успешная авторизация', async ({ page }) => {
-    /* const browser = await chromium.launch({
+    /* const browser = await chrome.launch({
       headless: false,
       slowMo: 500
     }); */ 
-    // Go to https://netology.ru/
     await page.goto('https://netology.ru/');
-    // Click text=Войти
-    await Promise.all([
-      page.waitForNavigation( /*{ url: 'https://netology.ru/?modal=sign_in' }*/),
-      page.click('text=Войти')
-    ]);
-    // Click [placeholder="Email"]
-    await page.click('[placeholder="Email"]');
-    // Fill [placeholder="Email"]
-    await page.fill('[placeholder="Email"]', emailValid);
-    // Click [placeholder="Пароль"]
-    await page.click('[placeholder="Пароль"]');
-    // Click [placeholder="Пароль"]
-    await page.fill('[placeholder="Пароль"]', passwordValid);
-    // Click [data-testid="login-submit-btn"]
-    await Promise.all([
-      page.waitForNavigation(/*{ url: 'https://netology.ru/profile/8581862' }*/),
-      page.click('[data-testid="login-submit-btn"]')
-    ]);
-    // await expect(page).toBeVisible("//* [data-testid='profile-programs-content']");  
-    await expect(page).toHaveText('Моё обучение');
-    /* // Click [data-testid="profile-programs-content"] >> text=Моё обучение
-    await page.click('[data-testid="profile-programs-content"] >> text=Моё обучение'); */
-    // ---------------------
-    /* await context.close();
-    await browser.close(); */
+    await page.getByRole('link', { name: 'Войти' }).click();
+    await page.getByPlaceholder('Email').click();
+    await page.getByPlaceholder('Email').fill(emailValid);
+    await page.getByPlaceholder('Пароль').fill(passwordValid);
+    await page.getByTestId('login-submit-btn').click();
+    await expect(page.getByText( 'Моё обучение' )).toBeVisible();
+    // await expect(page.getByText('Вы остановились здесь. Продолжим?')).toBeVisible();
   });
 
   test('Неуспешная авторизация', async ({ page }) => {
@@ -47,26 +28,13 @@ test.describe("Авторизация", () => {
       headless: false,
       slowMo: 500
     }); */
-    // Go to https://netology.ru/
     await page.goto('https://netology.ru/');
-    // Click text=Войти
-    await Promise.all([
-      page.waitForNavigation(/*{ url: 'https://netology.ru/?modal=sign_in' }*/),
-      page.click('text=Войти')
-    ]);
-      // Click [placeholder="Email"]
-    await page.click('[placeholder="Email"]');
-    // Fill [placeholder="Email"]
-    await page.fill('[placeholder="Email"]', emailInvalid);
-    // Click [placeholder="Пароль"]
-    await page.click('[placeholder="Пароль"]');
-    // Fill [placeholder="Пароль"]
-    await page.fill('[placeholder="Пароль"]', passwordInvalid);
-    // Click [data-testid="login-submit-btn"]
-    await page.click('[data-testid="login-submit-btn"]');
-    await expect(page).toBeVisible("//* [@data-testid='login-error-hint']");
-    /* await context.close();
-    await browser.close();*/
+    await page.getByRole('link', { name: 'Войти' }).click();
+    await page.getByPlaceholder('Email').click();
+    await page.getByPlaceholder('Email').fill(emailInvalid);
+    await page.getByPlaceholder('Пароль').fill(passwordInvalid);
+    await page.getByTestId('login-submit-btn').click();
+    await expect(page.getByTestId('login-error-hint')).toBeVisible();
   });
 });
 
